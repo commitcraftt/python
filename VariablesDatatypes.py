6f21e4ea36977e13af5cf52074fd19da56ecf6f3
TheArchitecct <salmanthearchitectt.in>
2025-12-23 19:01:21 +0530

    Learned about variables and datatypes

---
+++ VariablesDatatypes.py
@@ -0,0 +1,22 @@
+# A variable is a name that Refers to a value stored in a memory 
+name = "salman"
+age = 20
+# here name,age are variables
+
+
+# A data type defines what kind of value is stored 
+""" 
+1) Numeric datatypes
+-int
+-float
+ 
+2) Boolean 
+3) Sequence types
+-string
+-list
+-tuple
+4)set
+5) dictionary
+6) None
+
+"""
